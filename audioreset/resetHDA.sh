f92bded3286442c07f9f26006c608a502937a1c0
Michael Clifford <michael@cliffom.net>
2014-06-13 22:22:23 -0700

    Fixes to resetHDA.sh

--- audioreset/resetHDA.sh
+++ audioreset/resetHDA.sh
@@ -6,6 +6,6 @@
 #  Created by Michael Clifford on 6/13/14.
 #  Copyright (c) 2014 Suite Potato. All rights reserved.
 
-`which kextunload` /System/Library/Extensions/AppleHDA.kext
-`which kextunload` /System/Library/Extensions/AppleHDA.kext
+kextunload /System/Library/Extensions/AppleHDA.kext
+kextload /System/Library/Extensions/AppleHDA.kext
 killall coreaudiod
