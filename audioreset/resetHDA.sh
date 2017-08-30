97906e13abf167b3ae62c118611a313adf4cef88
Andrew Wyatt <andrew@fuduntu.org>
2017-08-30 07:34:28 -0400

    Cleaner reset seems to work better on at least my devices.

--- audioreset/resetHDA.sh
+++ audioreset/resetHDA.sh
@@ -6,6 +6,7 @@
 #  Created by Michael Clifford on 6/13/14.
 #  Copyright (c) 2014 Suite Potato. All rights reserved.
 
+launchctl unload /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist
 kextunload /System/Library/Extensions/AppleHDA.kext
 kextload /System/Library/Extensions/AppleHDA.kext
-killall coreaudiod
+launchctl load /System/Library/LaunchDaemons/com.apple.audio.coreaudiod.plist
